28deb9fe3f22ed07b8ed29b921b91a4288540b7c
damienmadaule <damienmadaule@free.fr>
2020-03-06 12:33:07 +0100

    Test 2 Refactor

--- src/test/java/TestUnitaire.java
+++ src/test/java/TestUnitaire.java
@@ -2,13 +2,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestUnitaire {
-    //teste si le nombre est not divisible par 3 ou 5
+    //test si le nombre est not divisible par 3 ou 5
     @Test
     public void should_print_number_if_not_divide_by_3_and_5(){
         Assertions.assertEquals(3, FizzBuzz.SayFizzBuzz(3));
     }
+    //test si le nombre est divisible par 3 et 5
     @Test
-    public void should_print_fizzbuzz_if_divide_by_3_and_5(int number){
+    public void should_print_fizzbuzz_if_divide_by_3_and_5(){
         Assertions.assertEquals("FizzBuzz", FizzBuzz.SayFizzBuzz(15));
     }
+
 }
