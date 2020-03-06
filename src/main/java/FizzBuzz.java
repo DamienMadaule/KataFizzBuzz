4bee14db3baef9c1d186116e1b22d96f3d057e14
damienmadaule <damienmadaule@free.fr>
2020-03-06 12:25:12 +0100

    init

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,5 @@
+public class FizzBuzz {
+    public static String SayFizzBuzz(int number) {
+        return String.valueOf(number);
+    };
+}
---
+++ src/test/java/TestUnitaire.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestUnitaire {
+    @Test
+    public void should_print_number_if_not_divide_by_3_and_5(){
+
+    }
+
+}
