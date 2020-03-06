ed2a5d0f1296005b962ad050b37ff08c05dd86d7
damienmadaule <damienmadaule@free.fr>
2020-03-06 12:26:55 +0100

    test 1 green

--- src/test/java/TestUnitaire.java
+++ src/test/java/TestUnitaire.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 public class TestUnitaire {
     @Test
     public void should_print_number_if_not_divide_by_3_and_5(){
-
+        Assertions.assertEquals(3, FizzBuzz.SayFizzBuzz(3));
     }
 
 }
